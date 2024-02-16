91e2cd1a6c196c11d227a5ca101577df8e2e48cd
danielbelle <henrique.danielb@gmail.com>
2024-02-16 17:47:59 -0300

    v4.4 - new adjusts commit3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App min-w-[420px] h-[100svh] w-screen">
+    <div className="App w-screen">
       <Nav />
       <Profile />
       <About />
